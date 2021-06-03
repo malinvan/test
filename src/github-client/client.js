b84334abcede6883a46d6798e156c3329b34824f
mmolliss <malin.vannesjo@gmail.com>
2021-06-03 08:55:26 +0200

    Removed cache

--- src/github-client/client.js
+++
@@ -1,4 +0,0 @@
-import { Octokit } from "octokit";
-
-// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-export const octokit = new Octokit({ auth: `personal-access-token123` });
